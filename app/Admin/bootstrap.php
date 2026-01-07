1258abc128d04d8fd3c1bcd727889c5d25d74d2e
wujp123 <wjp262043881@gmail.com>
2026-01-07 22:38:09 +0800

    +++

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-
+use Encore\Admin\Facades\Admin;
 /**
  * Laravel-admin - admin builder based on Laravel.
  * @author z-song <https://github.com/z-song>
@@ -19,3 +19,15 @@
  */
 
 Encore\Admin\Form::forget(['map', 'editor']);
+
+
+Admin::script(<<<'JS'
+$(function () {
+    // 展开当前激活菜单的所有父级
+    $('.sidebar-menu li.active')
+        .parents('.treeview')
+        .addClass('menu-open')
+        .children('.treeview-menu')
+        .show();
+});
+JS);
