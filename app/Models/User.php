e16d9deb596dcbbb2c79580b3d9085b75011d560
wujp123 <wjp262043881@gmail.com>
2026-01-09 01:12:34 +0800

    update cache

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,10 @@ class User extends Authenticatable
         'email',
         'password',
         'balance',
+        'tg_id',  // TG 的唯一数字 ID (注意：如果你数据库字段叫 tg_id，这里要改成 'tg_id')
+        'username',     // TG 的用户名 (@xxx)
+        'parent_id',    // 上级 ID (用于绑定邀请关系)
+        'avatar',       // 头像 (如果有)
     ];
 
     /**
@@ -54,5 +58,11 @@ class User extends Authenticatable
     {
         // 这里的 parent_id 是 users 表里的字段，指向 id
         return $this->belongsTo(User::class, 'parent_id', 'id');
+    }
+
+    // 关联：我的下级 (我邀请了谁) - 以后做推广系统会用到
+    public function children()
+    {
+        return $this->hasMany(User::class, 'parent_id', 'id');
     }
 }
