30d91445a7fbbc8de2b34fbc340fd440bac7d0a7
wujp123 <wjp262043881@gmail.com>
2026-01-08 23:08:14 +0800

    route update

--- routes/api.php
+++ routes/api.php
@@ -11,36 +11,32 @@ use App\Http\Controllers\WebhookController;
 // Telegram 登录
 Route::post('/auth/telegram', [AuthController::class, 'telegramLogin']);
 
-Route::post('/game/spin', [GameController::class, 'spin']);
-//// 旋转老虎机
-Route::get('/game/balance', [GameController::class, 'balance']);
-
-Route::get('game/config', [GameController::class, 'config']);
-
-// ★★★ 2. 支付相关 (新增这几行) ★★★
-Route::post('deposit', [PaymentController::class, 'deposit']);   // 充值
-Route::post('withdraw', [PaymentController::class, 'withdraw']); // 提现
 Route::any('notify', [PaymentController::class, 'notify']);//回调
-Route::get('deposit/channels', [PaymentController::class, 'channels']);//通道
-Route::post('/telegram/webhook', [BotController::class, 'webhook']);
+
+Route::post('/telegram/webhook', [BotController::class, 'webhook']);// TG 机器人消息回调
 
 Route::post('/webhook/nowpayments', [WebhookController::class, 'handleNowPayments'])->name('webhook.nowpayments');
 // 应用中间件 tg.auth
 Route::middleware(['tg.auth'])->group(function () {
 
-    // 获取用户信息
-//    Route::get('/user/info', function (Request $request) {
-//        return $request->user();
-//    });
-    // 暂时放出来方便测试，正式上线请放回 middleware 里面
-//    Route::post('/game/spin', [GameController::class, 'spin']);
-    // 旋转老虎机
-//    Route::get('/user/balance', [GameController::class, 'balance']);
-
-//    Route::get('config', [GameController::class, 'config']);
-
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    // ...
+    Route::post('/game/spin', [GameController::class, 'spin']);
+    // 1. 游戏核心逻辑 (必须知道扣谁的钱)
+    Route::get('/game/balance', [GameController::class, 'balance']);
+    Route::get('game/config', [GameController::class, 'config']);
+
+    // ★★★ 2. 支付相关 (新增这几行) ★★★
+    Route::post('deposit', [PaymentController::class, 'deposit']);   // 充值
+    Route::post('withdraw', [PaymentController::class, 'withdraw']); // 提现
+
+    // 3. 辅助接口
+    // 支付通道虽然看似通用，但通常建议登录后才展示（可能根据 VIP 等级显示不同通道）
+    Route::get('deposit/channels', [PaymentController::class, 'channels']);
+
+    // 4. 用户信息
+    Route::get('/user/info', function (Request $request) {
+        return $request->user();
+    });
 });
